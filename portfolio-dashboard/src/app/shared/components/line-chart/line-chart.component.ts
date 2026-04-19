0ec9df26f148d510224a5277405eebfa0425b13d
Andre McMorris <ajmcmorris@gmail.com>
2026-04-19 18:54:00 -0400

    feat: added new shared components

---
+++ portfolio-dashboard/src/app/shared/components/line-chart/line-chart.component.ts
@@ -0,0 +1,117 @@
+import { Component } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+export interface RowItem {
+  entries: [];
+  value: { name: string };
+}
+
+@Component({
+  selector: 'app-line-chart',
+  imports: [NgxChartsModule],
+  templateUrl: './line-chart.component.html',
+  styleUrl: './line-chart.component.scss',
+})
+export class LineChartComponent {
+  multi = [
+    {
+      name: 'Germany',
+      series: [
+        {
+          name: '1990',
+          value: 62000000,
+        },
+        {
+          name: '2010',
+          value: 73000000,
+        },
+        {
+          name: '2011',
+          value: 89400000,
+        },
+      ],
+    },
+
+    {
+      name: 'USA',
+      series: [
+        {
+          name: '1990',
+          value: 250000000,
+        },
+        {
+          name: '2010',
+          value: 309000000,
+        },
+        {
+          name: '2011',
+          value: 311000000,
+        },
+      ],
+    },
+
+    {
+      name: 'France',
+      series: [
+        {
+          name: '1990',
+          value: 58000000,
+        },
+        {
+          name: '2010',
+          value: 50000020,
+        },
+        {
+          name: '2011',
+          value: 58000000,
+        },
+      ],
+    },
+    {
+      name: 'UK',
+      series: [
+        {
+          name: '1990',
+          value: 57000000,
+        },
+        {
+          name: '2010',
+          value: 62000000,
+        },
+      ],
+    },
+  ];
+  view: number[] = [700, 300];
+
+  // options
+  legend = true;
+  showLabels = true;
+  animations = true;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Year';
+  yAxisLabel = 'Population';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+  };
+
+  constructor() {
+    // Object.assign(this, { this.multi });
+  }
+
+  onSelect(data: RowItem): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: RowItem): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: RowItem): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
