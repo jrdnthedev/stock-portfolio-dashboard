08e5b7079304851d6433c2beca4eaafce67ebf26
Andre McMorris <ajmcmorris@gmail.com>
2026-04-19 10:08:02 -0400

    ci: fixed route paths

--- portfolio-dashboard/src/app/app.routes.ts
+++ portfolio-dashboard/src/app/app.routes.ts
@@ -3,18 +3,22 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.DashboardComponent),
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then((m) => m.DashboardComponent),
     title: 'Dashboard',
   },
   {
     path: 'holdings',
-    loadComponent: () => import('./features/holdings/holdings').then((m) => m.HoldingsComponent),
+    loadComponent: () =>
+      import('./features/holdings/holdings.component').then((m) => m.HoldingsComponent),
     title: 'Holdings',
   },
   {
     path: 'ticker-detail',
     loadComponent: () =>
-      import('./features/ticker-details/ticker-details').then((m) => m.TickerDetailsComponent),
+      import('./features/ticker-details/ticker-details.component').then(
+        (m) => m.TickerDetailsComponent
+      ),
     title: 'Ticker Detail',
   },
 ];
