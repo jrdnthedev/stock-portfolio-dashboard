7ceb988d89e246198c8af66632fe02fccd55552c
Andre McMorris <ajmcmorris@gmail.com>
2026-04-19 09:56:40 -0400

    ci: added hydration to configuration

--- portfolio-dashboard/src/app/app.routes.ts
+++ portfolio-dashboard/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.DashboardComponent),
+    title: 'Dashboard',
+  },
+  {
+    path: 'holdings',
+    loadComponent: () => import('./features/holdings/holdings').then((m) => m.HoldingsComponent),
+    title: 'Holdings',
+  },
+  {
+    path: 'ticker-detail',
+    loadComponent: () =>
+      import('./features/ticker-details/ticker-details').then((m) => m.TickerDetailsComponent),
+    title: 'Ticker Detail',
+  },
+];
