3569df579f1afe522ede8be5d114544bd345394a
Andre McMorris <ajmcmorris@gmail.com>
2026-04-25 12:22:40 -0400

    feat: added dropdown component

---
+++ portfolio-dashboard/src/app/shared/components/dropdown/dropdown.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  imports: [CommonModule],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.scss',
+})
+export class DropdownComponent {
+  data: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  @Input() name = 'test';
+  @Input() label = 'Select an option';
+  @Input() isStacked = false;
+  selectionChanged = output<string>();
+
+  onSelectionChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+    this.selectionChanged.emit(selectedValue);
+  }
+}
